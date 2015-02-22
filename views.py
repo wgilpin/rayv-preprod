908718b93eb457b7f5e16a6115afd2cc2d8cce6d
wgilpin <wgilpin@gmail.com>
2015-02-22 18:56:48 +0000

    bug in logging

--- views.py
+++ views.py
@@ -63,7 +63,10 @@ def get_user_votes(current_user_id, user_id, since=None):
   votes = {}
   if 'v' in user_dict:
     votes = user_dict['v']
-    logging.debug("get_user_votes: from memcache %d votes"%len(votes))
+    if votes:
+      logging.debug("get_user_votes: from memcache %d votes"%len(votes))
+    else:
+      logging.debug("get_user_votes: from memcache no votes")
   if len(votes) == 0 or not 'v' in user_dict:
     # we are going to memcache the votes sowe get ALL votes & ignore since
     votes = Vote.get_user_votes(user_id)
